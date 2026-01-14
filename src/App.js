7086ef098ff7ca8dd9191ede2cd7151e4400b20f
Rishi Shori <rshori@aecordigital.com>
2026-01-14 10:02:21 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <h1>Hello!!!!!</h1>
         <p>This is a React app running on AWS ECS</p>
         <p>Deployed via Docker and ECR</p>
       </header>
